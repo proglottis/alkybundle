5d82b5cbaa79596d753808c4c8609e5303772448
James Fargher <proglottis@nothing.co.nz>
2009-02-08 15:07:23 +0000

    Initial layout

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+// Entry Point.
+
+// Copyright (C) 2009  James Fargher
+
+// Distributed under the GNU General Public License, Version 3.
+// See accompanying file gpl-3.0.txt or copy at
+//   <http://www.gnu.org/licenses/>
+
+int main(int argc, char* argv[])
+{
+  return 0;
+}

270c2fc79316ca2454391fdbc24ca8bcf438f4a0
James Fargher <proglottis@nothing.co.nz>
2009-03-14 13:32:12 +0000

    Change UI to use SQLite ingredient bundle

--- src/ui/qt/mainwindowfactory.cpp
+++ src/ui/qt/mainwindowfactory.cpp
@@ -10,17 +10,30 @@
 
 #include "ingredientmodel.hpp"
 #include "defaultmainwindow.hpp"
+#include "resource/home.hpp"
+#include "bundle/sqliteingredientbundle.hpp"
+#include "locale/stringconvert.hpp"
 
 namespace alky {
 namespace ui {
 namespace qt {
 
 using alky::bundle::IngredientBundle;
+using alky::bundle::SQLiteIngredientBundle;
 using alky::cocktail::Ingredient;
+using alky::resource::Home;
+using alky::locale::StringConvert;
 
 MainWindow *MainWindowFactory::create(QWidget *parent)
 {
-  boost::shared_ptr<IngredientBundle> ingBundle(IngredientBundle::create());
+  boost::shared_ptr<StringConvert> convert(StringConvert::create());
+  boost::shared_ptr<Home> home(Home::create());
+  // TODO: Create sqlite manager
+  sqlite3* db;
+  sqlite3_open(convert->wide_to_utf8(
+      home->config_dir() + L"/bundle.db").c_str(), &db);
+  boost::shared_ptr<IngredientBundle> ingBundle(
+    new SQLiteIngredientBundle(db));
   boost::shared_ptr<IngredientModel> ingModel(new IngredientModel(ingBundle));
   ingBundle->add_listener(ingModel);
   boost::shared_ptr<IngredientDialogFactory> ingFac(
